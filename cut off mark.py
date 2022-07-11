7955b2b74b77fccdc9a118c946deda51e0749d78
Stonetech66 <106387735+Stonetech66@users.noreply.github.com>
2022-07-11 02:19:33 +0100

    Update cut off mark.py

--- cut off mark.py
+++ cut off mark.py
@@ -1,7 +1,7 @@
 #-*-coding:utf8;-*-
 #qpy:console
-a1,b2,b3,c4,c5,c6= 4.0,3.6,3.2,2.8,2.4,2.0
-add,a,f,c,d,e= 0,0,0,0,0,0
+a1, b2, b3, c4, c5, c6= 4.0, 3.6, 3.2, 2.8, 2.4, 2.0
+add, a, f, c, d, e= 0, 0, 0, 0, 0, 0
 for i in range(5):
 
  waec= input("enter your waec score: ")
@@ -43,5 +43,6 @@ print("        jamb score: ",jamb)
 print("        post utme score: ",post_utme)
 agg=agg_w+jamb/8+post_utme
 print("\n\n   Your aggregate is: ",agg)
+
 if agg < 50:
   print ("You failed to qualify")
