5652ad519f9dfcb9c3ba7991aaf581c99243684f
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2022-07-12 12:07:39 +0100

    Update cut off mark.py

--- cut off mark.py
+++ cut off mark.py
@@ -8,36 +8,45 @@ for i in range(5):
  if waec == "A1":
    add += 1
    A1= 4.0
+   
  elif waec == "B2":
    a += 1
    B2= 3.6   
+   
  elif waec == "B3":
    f += 1
    B3= 3.2
+   
  elif waec == "C4":
    c += 1
    
  elif waec == "C5":
    d +=1
    C5= 3.6
+   
  elif waec == "C6":
    e += 1
    C6= 3.6
+   
  else:
    print("sorry you don't qualify Unilag")
+   
 agg_w=add*a1+a*b2+f*b3+c*c4+d*c5+e*c6
+
 for i in range (3):
  jamb = int(input("enter your jamb score: "))
  if jamb < 400:
      break
  elif jamb >400:
     print("exceeds range")
+   
 for i in range(3):
  post_utme= float(input("enter your post utme score: "))
  if post_utme< 30:
      break
  elif post_utme> 30:
-    print("exceeds range")
+    print("ivalid input")
+   
 print("\n\n  waec score: ",agg_w)
 print("        jamb score: ",jamb)
 print("        post utme score: ",post_utme)
@@ -45,4 +54,4 @@ agg=agg_w+jamb/8+post_utme
 print("\n\n   Your aggregate is: ",agg)
 
 if agg < 50:
-  print ("You failed to qualify")
+  print ("You have a very low chance of entering unilag")
